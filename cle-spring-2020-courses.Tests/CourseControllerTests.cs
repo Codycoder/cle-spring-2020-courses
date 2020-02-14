869e4f04be039e86e692006d8ce4c1c0441be503
Jen E <jenevans3ten@gmail.com>
2020-02-14 09:20:59 -0500

    skeleton of asp.net web app with xunit test project

---
+++ cle-spring-2020-courses.Tests/CourseControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace cle_spring_2020_courses.Tests
+{
+    public class CourseControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
