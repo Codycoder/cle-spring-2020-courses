13ecf231a324a14bfa94b9e74a8298aa1ae40e8d
Jen E <jenevans3ten@gmail.com>
2020-02-14 10:52:10 -0500

    added failing test to pass course model to controller index view

--- cle-spring-2020-courses.Tests/CourseControllerTests.cs
+++ cle-spring-2020-courses.Tests/CourseControllerTests.cs
@@ -2,23 +2,37 @@ using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using cle_spring_2020_courses.Controllers;
+using cle_spring_2020_courses.Models;
 
 namespace cle_spring_2020_courses.Tests
 {
     public class CourseControllerTests
     {
+        CourseController controller;
+        
+        public CourseControllerTests()
+        {
+            controller = new CourseController();
+        }
+
         [Fact]
         public void Index_Returns_A_View()
         {
-            // Arrange
-            var controller = new CourseController();
-
             // Act
             var result = controller.Index();
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
 
+        [Fact]
+        public void Index_Passes_CourseModel_To_View()
+        {
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<CourseModel>(result.Model);
         }
     }
 }
---
+++ cle-spring-2020-courses/Models/CourseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_spring_2020_courses.Models
+{
+    public class CourseModel
+    {
+    }
+}
