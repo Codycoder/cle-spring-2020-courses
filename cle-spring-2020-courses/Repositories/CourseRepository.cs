282a0860424b2012a3300c9f80350330b41422d6
Jen E <jenevans3ten@gmail.com>
2020-02-14 12:00:16 -0500

    added repository pattern

---
+++ cle-spring-2020-courses/Repositories/CourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using cle_spring_2020_courses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_spring_2020_courses.Repositories
+{
+    public class CourseRepository : IRepository<CourseModel>
+    {
+        public IEnumerable<CourseModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cle-spring-2020-courses/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_spring_2020_courses.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+    }
+}
