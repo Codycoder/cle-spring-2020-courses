89b741bf1c229970de37f9d577d6a3171eebcbd2
Jen E <jenevans3ten@gmail.com>
2020-02-19 14:00:42 -0500

    added University dBcontext and migrated to sql database

---
+++ cle-spring-2020-courses/Migrations/20200219184554_NewDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cle_spring_2020_courses.Migrations
+{
+    public partial class NewDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Machine learning for humans", "Machine Learning" });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "It's all you need to learn it all", "C# for Smarties" });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "OMG, the front end will become your BFF", "HTML, CSS, JS, oh my!" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
---
+++ cle-spring-2020-courses/UniversityContext.cs
@@ -0,0 +1,53 @@
+﻿using cle_spring_2020_courses.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_spring_2020_courses
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=UniversityDB_Spring20;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().HasData(
+                new Course()
+                {
+                    Id = 1,
+                    Name = "Machine Learning",
+                    Description = "Machine learning for humans"
+                },
+
+                new Course()
+                {
+                    Id = 2,
+                    Name = "C# for Smarties",
+                    Description = "It's all you need to learn it all"
+                },
+
+                new Course()
+                {
+                    Id = 3,
+                    Name = "HTML, CSS, JS, oh my!",
+                    Description = "OMG, the front end will become your BFF"
+                });
+
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
